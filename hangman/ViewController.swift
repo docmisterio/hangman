39ebd349e76ebaf61167ecb3b9f9332a0d40ffc2
Zach Becker <doc.misterio@gmail.com>
2020-12-11 22:59:51 -0600

    figured out how to dig into the data after an API call. Also learned I can use the structs to build the structure of the data coming in but only USE the one I need to store the data. let the others die in the stack.

--- hangman/ViewController.swift
+++ hangman/ViewController.swift
@@ -2,47 +2,50 @@ import UIKit
 
 class ViewController: UIViewController {
     let buttonsView = UIView()
+    let wordGuessView = UITextField()
     var letterButtons = [UIButton]()
-    
     
     
     override func loadView() {
         super.loadView()
         
         buttonsView.translatesAutoresizingMaskIntoConstraints = false
-        buttonsView.backgroundColor = UIColor.gray
+        buttonsView.backgroundColor = UIColor.lightGray
         buttonsView.layer.cornerRadius = 25
         view.addSubview(buttonsView)
         
-        let width = 60
+        let width = 61
         let height = 100
         
         for row in 0..<2 {
             for column in 0..<13 {
                 let button = UIButton()
-                button.titleLabel?.font = UIFont.systemFont(ofSize: 36)
-                button.setTitle("W", for: .normal)
-                button.addTarget(self, action: #selector(letterTapped), for: .touchUpInside)
-                
                 let frame = CGRect(x: column * width, y: row * height, width: width, height: height)
+                
                 button.frame = frame
+                button.setTitle("A", for: .normal)
+                button.titleLabel?.font = UIFont.systemFont(ofSize: 36)
+                button.addTarget(self, action: #selector(letterTapped), for: .touchUpInside)
                 
                 buttonsView.addSubview(button)
                 letterButtons.append(button)
             }
         }
+        
+        wordGuessView.translatesAutoresizingMaskIntoConstraints = false
+        wordGuessView.isUserInteractionEnabled = false
+        wordGuessView.font = UIFont.systemFont(ofSize: 44)
+        wordGuessView.layer.cornerRadius = 25
+        wordGuessView.placeholder = "your word here".uppercased()
+        wordGuessView.textAlignment = .center
+        wordGuessView.backgroundColor = .systemGray6
+        view.addSubview(wordGuessView)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+    
         setupConstraints()
-        
-        
-        
-        
-        
-        
     }
     
     @objc func letterTapped() {
@@ -56,15 +59,15 @@ class ViewController: UIViewController {
             buttonsView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
             buttonsView.heightAnchor.constraint(equalToConstant: 200),
             
-            
-            
+            wordGuessView.bottomAnchor.constraint(equalTo: buttonsView.topAnchor, constant: -20),
+            wordGuessView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            wordGuessView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            wordGuessView.heightAnchor.constraint(equalToConstant: 75),
             
         ]
         
         NSLayoutConstraint.activate(constraints)
     }
-    
-    
 }
 
 
